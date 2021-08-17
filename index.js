4144c19ecfd9e9e3ff1951d6ee5d96bba7be3fd8
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2021-08-17 09:41:35 -0500

    added skill section, working

---
+++ index.js
@@ -0,0 +1,26 @@
+function openSkill(evt, skillName){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName('tabcontent');
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName('tablinks');
+    for(i = 0; i < tablinks.length; i++){
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
+    }
+    document.getElementById(skillName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// function openCity(evt, skillName) {
+//     var i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = "none";
+//     }
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+//   }
